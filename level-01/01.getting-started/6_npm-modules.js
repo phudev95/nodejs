269865a9cdf1778512235fbf58a5b2acb6a0ff77
Phu Phan <phudev95@gmail.com>
2016-10-16 18:08:56 +0700

    [6_npm_modules.js] Change url website

--- level-01/01.getting-started/6_npm-modules.js
+++ level-01/01.getting-started/6_npm-modules.js
@@ -1,6 +1,6 @@
 var request = require('request');
 
-request('http://www.phasnvanphu.com', function(err, status, data) {
+request('http://www.phanvanphu.com', function(err, status, data) {
     if (!err && status.statusCode == 200) {
         console.log("================");
         console.log(data);

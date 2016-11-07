8d0350522a063293a8e63eaf2a501e67e978bd4d
Phu Phan <phudev95@gmail.com>
2016-11-07 18:35:42 +0700

    Update test case

--- level-03/01.testing-strategies/test/mission_application_spec.js
+++ level-03/01.testing-strategies/test/mission_application_spec.js
@@ -39,4 +39,76 @@ describe('Membership application requitements', function () {
             assert(validApp.nameIsValid());
         });
     });
+
+    describe('Application invalid if...', function () {
+        it('Email is 4 characters or less', function () {
+            var app = new MembershipApplication({email: 'g@g'});
+            assert(!app.emailIsValid());
+        });
+
+        it('email does not contain an @', function () {
+            var app = new MembershipApplication({email: "thingthingthing:thing.com"});
+            assert(!app.emailIsValid());
+        });
+
+        it('email is omitted', function () {
+            var app = new MembershipApplication();
+            assert(!app.emailIsValid());
+        });
+
+        it('height is less than 60 inches', function () {
+            var app = new MembershipApplication({height: 10});
+            assert(!app.heightIsValid());
+        });
+
+        it('height is more than 75 inches', function () {
+            var app = new MembershipApplication({height: 80});
+            assert(!app.heightIsValid());
+        });
+
+        it('height is omitted', function () {
+            var app = new MembershipApplication();
+            assert(!app.heightIsValid());
+        });
+
+        it('age is more than 100', function () {
+            var app = new MembershipApplication({age: 101});
+            assert(!app.ageIsValid());
+        });
+
+        it('age less than 15', function () {
+            var app = new MembershipApplication({age: 14});
+            assert(!app.ageIsValid());
+        });
+
+        it('age is omitted', function () {
+            var app = new MembershipApplication();
+            assert(!app.ageIsValid());
+        });
+
+        it('weight less than 100', function () {
+            var app = new MembershipApplication({weight: 99});
+            assert(!app.weightIsValid());
+        });
+
+        it('weight less more than 300', function () {
+            var app = new MembershipApplication({weight: 301});
+            assert(!app.weightIsValid());
+        });
+
+        it('weight is omitted', function () {
+            var app = new MembershipApplication();
+            assert(!app.weightIsValid());
+        });
+
+        it('first is omitted', function () {
+            var app = new MembershipApplication();
+            assert(!app.nameIsValid());
+        });
+
+        it('last is omitted', function () {
+            var app = new MembershipApplication();
+            assert(!app.nameIsValid());
+        });
+    });
 });

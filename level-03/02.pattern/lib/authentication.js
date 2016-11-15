ae3d604c4db0a7f7d9e67ddf81a3d000d2dce19c
Phu Phan <phudev95@gmail.com>
2016-11-15 17:53:56 +0700

    Debug: Get flow of design pattern auth_spec.js (That's combination of Registration vs )Authentication - Registration: Use it to register a user - Authentication: Use it to check user is valid (email, password)

--- level-03/02.pattern/lib/authentication.js
+++ level-03/02.pattern/lib/authentication.js
@@ -6,131 +6,154 @@ var Log = require("../models/log");
 var assert = require("assert");
 
 //A wrapper object for the result of Authentication
-var AuthResult =function(creds){
-  var result = {
-    creds : creds,
-    success : false,
-    message : "Invalid email or password",
-    user : null,
-    log : null
-  };
+var AuthResult = function (creds) {
+    var result = {
+        creds: creds,
+        success: false,
+        message: "Invalid email or password",
+        user: null,
+        log: null
+    };
 
-  return result;
+    return result;
 };
 
 //The prototype which our module here will export. Takes a db instance
 var Authentication = function (db) {
-  var self = this;
-  var continueWith = null;
-  events.EventEmitter.call(self);
+    var self = this;
+    var continueWith = null;
+    events.EventEmitter.call(self);
 
-  //validate credentials
-  var validateCredentials = function(authResult){
-    if(authResult.creds.email && authResult.creds.password){
-      self.emit("creds-ok", authResult);
-    }else{
-      self.emit("invalid",authResult);
-    }
-  };
-
-  //find the user
-  var findUser = function(authResult){
-    db.users.first({email : authResult.creds.email}, function(err,found){
-      assert.ok(err === null, err);
-      if(found){
-        authResult.user = new User(found);
-        self.emit("user-found",authResult);
-      }else{
-        self.emit("invalid",authResult);
-      }
-    })
-  };
-
-  //compare the password
-  var comparePassword = function(authResult){
-    var matched = bc.compareSync(authResult.creds.password,authResult.user.hashedPassword);
-    if(matched){
-      self.emit("password-accepted",authResult);
-    }else{
-      self.emit("invalid",authResult);
-    }
-  };
-
-  //bump the stats
-  var updateUserStats = function(authResult){
-
-    //set the updates on the authResult
-    var user = authResult.user;
-    user.signInCount+=1;
-    user.lastLoginAt = user.currentLoginAt;
-    user.currentLoginAt = new Date();
-
-    //now save them
-    var updates = {
-      signInCount : user.signInCount,
-      lastLoginAt : user.lastLoginAt,
-      currentLoginAt : user.currentLoginAt
+    // validate credentials
+    // self.on("login-received", validateCredentials);
+    var validateCredentials = function (authResult) {
+        if (authResult.creds.email && authResult.creds.password) {
+            self.emit("creds-ok", authResult);
+        } else {
+            self.emit("invalid", authResult);
+        }
     };
-    db.users.updateOnly(updates,authResult.user.id,function(err,updates){
-      assert.ok(err === null, err);
-      self.emit("stats-updated",authResult);
-    });
-  };
 
-  //create a log entry
-  var createLog = function(authResult){
+    // find the user
+    // self.on("creds-ok", findUser);
+    var findUser = function (authResult) {
+        db.users.first({email: authResult.creds.email}, function (err, found) {
+            assert.ok(err === null, err);
+            if (found) {
+                authResult.user = new User(found);
+                self.emit("user-found", authResult);
+            } else {
+                self.emit("invalid", authResult);
+            }
+        })
+    };
 
-    var log = new Log({subject : "Authentication",
-      userId : authResult.user.id,
-      entry : "Successfully logged in"
-    });
+    // compare the password
+    // self.on("user-found", comparePassword);
+    var comparePassword = function (authResult) {
+        var matched = bc.compareSync(authResult.creds.password, authResult.user.hashedPassword);
+        if (matched) {
+            self.emit("password-accepted", authResult);
+        } else {
+            self.emit("invalid", authResult);
+        }
+    };
 
-    db.logs.save(log,function(err,newLog){
-      authResult.log = newLog;
-      self.emit("log-created",authResult);
-    });
+    // bump the stats
+    // self.on("password-accepted", updateUserStats);
+    var updateUserStats = function (authResult) {
 
-  };
+        //set the updates on the authResult
+        var user = authResult.user;
+        user.signInCount += 1;
+        user.lastLoginAt = user.currentLoginAt;
+        user.currentLoginAt = new Date();
 
-  //If everything goes well, this will be called
-  var authOk = function(authResult){
-    authResult.success = true;
-    authResult.message = "Welcome!";
-    self.emit("authenticated",authResult);
-    self.emit("completed",authResult);
-    if(continueWith){
-      continueWith(null,authResult);
-    }
-  };
+        //now save them
+        var updates = {
+            signInCount: user.signInCount,
+            lastLoginAt: user.lastLoginAt,
+            currentLoginAt: user.currentLoginAt
+        };
+        db.users.updateOnly(updates, authResult.user.id, function (err, updates) {
+            assert.ok(err === null, err);
+            self.emit("stats-updated", authResult);
+        });
+    };
 
-  //if anything fails this will be called
-  var authNotOk = function(authResult){
-    authResult.success = false;
-    self.emit("not-authenticated",authResult);
-    self.emit("completed",authResult);
-    if(continueWith){
-      continueWith(null,authResult);
-    }
-  };
+    // create a log entry
+    // self.on("stats-updated", createLog);
+    var createLog = function (authResult) {
 
-  //The event chain for our auth procedure
-  self.on("login-received", validateCredentials);
-  self.on("creds-ok", findUser);
-  self.on("user-found",comparePassword);
-  self.on("password-accepted",updateUserStats);
-  self.on("stats-updated",createLog);
-  self.on("log-created",authOk);
+        var log = new Log({
+            subject: "Authentication",
+            userId: authResult.user.id,
+            entry: "Successfully logged in"
+        });
 
-  //The event chain for auth failure
-  self.on("invalid",authNotOk);
+        db.logs.save(log, function (err, newLog) {
+            authResult.log = newLog;
+            self.emit("log-created", authResult);
+        });
 
-  //Entry point - the only thing exported on our module
-  self.authenticate = function(creds,next){
-    continueWith = next;
-    var authResult = new AuthResult(creds);
-    self.emit("login-received", authResult);
-  };
+    };
+
+    // If everything goes well, this will be called
+    // self.on("log-created", authOk);
+    var authOk = function (authResult) {
+        authResult.success = true;
+        authResult.message = "Welcome!";
+        self.emit("authenticated", authResult);
+        self.emit("completed", authResult);
+        if (continueWith) {
+            continueWith(null, authResult);
+        }
+    };
+
+    // if anything fails this will be called
+    // self.on("invalid", authNotOk);
+    var authNotOk = function (authResult) {
+        authResult.success = false;
+        self.emit("not-authenticated", authResult);
+        self.emit("completed", authResult);
+        if (continueWith) {
+            continueWith(null, authResult);
+        }
+    };
+
+    //The event chain for our auth procedure
+    self.on("login-received", validateCredentials);
+    self.on("creds-ok", findUser);
+    self.on("user-found", comparePassword);
+    self.on("password-accepted", updateUserStats);
+    self.on("stats-updated", createLog);
+    self.on("log-created", authOk);
+
+    //The event chain for auth failure
+    self.on("invalid", authNotOk);
+
+    // Entry point - the only thing exported on our module
+    // Call from auth_spec.js
+    self.authenticate = function (creds, next) {
+        continueWith = next;
+
+        // {email: "test@test.com", password: "password"}
+        /*
+            {
+                creds: { email: 'test@test.com', password: 'password' },
+                success: false,
+                message: 'Invalid email or password',
+                user: null,
+                log: null
+            }
+        */
+        var authResult = new AuthResult(creds);
+
+        // => validateCredentials
+        self.emit("login-received", authResult);
+    };
 
 };
+
 util.inherits(Authentication, events.EventEmitter);
 module.exports = Authentication;
